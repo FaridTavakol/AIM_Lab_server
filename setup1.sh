e507b7b4ca7f016cb86c98a30803905af6ff387a
Farid <ftavakkolmoghadd@wpi.edu>
2020-03-31 12:19:19 -0400

    Added the libyaml-cpp-dev dependency

--- setup1.sh
+++ setup1.sh
@@ -1,18 +1,17 @@
 apt-get update
 apt-get upgrade -y
 
-apt-get install git-extras -y
-apt-get install virtualenv -y
-apt-get install cmake-curses-gui -y
-apt-get install python3-dev -y
-apt-get install -y python3-pip -y
-apt-get install libssl-dev libffi-dev -y
-apt-get install -y python3-venv -y
-apt-get install cmake-curses-gui -y
+apt-get install -y git-extras
+apt-get install -y virtualenv
+apt-get install -y cmake-curses-gui
+apt-get install -y python3-dev
+apt-get install -y python3-pip
+apt-get install -y libssl-dev libffi-dev
+apt-get install -y libyaml-cpp-dev
+apt-get install -y python3-venv
+apt-get install -y cmake-curses-gui
 
-
-
-apt install libasound2-dev -y
+apt install -y libasound2-dev
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 apt update
